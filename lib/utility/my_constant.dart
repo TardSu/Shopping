bc32f3e67ea74c88a1e2b8ba7d7888c4ca84a872
TardSu <everyday.tard@hotmail.com>
2021-12-10 20:38:41 +0700

    Ep7

---
+++ lib/utility/my_constant.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MyConstant {
+  // General
+  static String appName = 'Shopping';
+
+  // Route
+  static String routeAuthen = '/authen';
+  static String routeCreateAccount = '/createAccount';
+  static String routebuyerService = 'buyerService';
+  static String routeSalerService = 'salerService';
+  static String routeRiderService = 'riderService';
+}
