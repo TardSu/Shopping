bd0af5914e87e563221b46f1763bdd20c218e1fa
TardSu <everyday.tard@hotmail.com>
2021-12-17 17:00:09 +0700

    Ep12

--- lib/states/create_account.dart
+++ lib/states/create_account.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shopping/utility/my_constant.dart';
+import 'package:shopping/widgets/show_title.dart';
 
 class CreateAccount extends StatefulWidget {
   const CreateAccount({Key? key}) : super(key: key);
@@ -8,8 +10,139 @@ class CreateAccount extends StatefulWidget {
 }
 
 class _CreateAccountState extends State<CreateAccount> {
+  String? typeUser;
+
+  Row buildName(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          margin: EdgeInsets.only(top: 16),
+          width: size * 0.6,
+          child: TextFormField(
+            decoration: InputDecoration(
+              labelStyle: MyConstant().h3Style(),
+              labelText: 'Name : ',
+              prefixIcon: Icon(
+                Icons.fingerprint,
+                color: MyConstant.dark,
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: MyConstant.dark),
+                borderRadius: BorderRadius.circular(30),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: MyConstant.light),
+                borderRadius: BorderRadius.circular(30),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    double size = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Create New Account'),
+        backgroundColor: MyConstant.primary,
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(16),
+        children: [
+          buildTitle('ข้อมูลทั่วไป :'),
+          buildName(size),
+          buildTitle('ชนิดของ User :'),
+          buildRadioBuyer(size),
+          buildRadioSeller(size),
+          buildRadioRider(size),
+        ],
+      ),
+    );
+  }
+
+  Row buildRadioBuyer(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          width: size * 0.6,
+          child: RadioListTile(
+            value: 'buyer',
+            groupValue: typeUser,
+            onChanged: (value) {
+              setState(() {
+                typeUser = value as String?;
+              });
+            },
+            title: ShowTitle(
+              title: 'ผู้ซื้อ (buyer)',
+              textStyle: MyConstant().h3Style(),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Row buildRadioSeller(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          width: size * 0.6,
+          child: RadioListTile(
+            value: 'seller',
+            groupValue: typeUser,
+            onChanged: (value) {
+              setState(() {
+                typeUser = value as String?;
+              });
+            },
+            title: ShowTitle(
+              title: 'ผู้ขาย (seller)',
+              textStyle: MyConstant().h3Style(),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Row buildRadioRider(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          width: size * 0.6,
+          child: RadioListTile(
+            value: 'rider',
+            groupValue: typeUser,
+            onChanged: (value) {
+              setState(() {
+                typeUser = value as String?;
+              });
+            },
+            title: ShowTitle(
+              title: 'ผู้ส่ง (rider)',
+              textStyle: MyConstant().h3Style(),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Container buildTitle(String title) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 16),
+      child: ShowTitle(
+        title: title,
+        textStyle: MyConstant().h2Style(),
+      ),
+    );
   }
 }
--- lib/utility/my_constant.dart
+++ lib/utility/my_constant.dart
@@ -7,9 +7,9 @@ class MyConstant {
   // Route
   static String routeAuthen = '/authen';
   static String routeCreateAccount = '/createAccount';
-  static String routebuyerService = 'buyerService';
-  static String routeSalerService = 'salerService';
-  static String routeRiderService = 'riderService';
+  static String routebuyerService = '/buyerService';
+  static String routeSalerService = '/salerService';
+  static String routeRiderService = '/riderService';
 
   // Image
   static String image1 = 'images/image1.png';
@@ -37,5 +37,12 @@ class MyConstant {
         fontSize: 14,
         color: dark,
         fontWeight: FontWeight.normal,
+      );
+
+  ButtonStyle myButtonStyle() => ElevatedButton.styleFrom(
+        primary: MyConstant.primary,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
+        ),
       );
 }
